195d4582bb7be695a0b4b709facd0e3bc0faad01
hp0404 <hryhorii.pavlenko@gmail.com>
2021-09-22 11:53:45 +0300

    specify tmp dir, remove raw/temp data from version control

--- process.sh
+++ process.sh
@@ -2,5 +2,5 @@
 
 for pdf in data/raw_pdfs/*.pdf;
 do
-    python s2orc-doc2json/doc2json/grobid2json/process_pdf.py -i $pdf -o data/processed_pdfs
+    python s2orc-doc2json/doc2json/grobid2json/process_pdf.py -i $pdf -t data/tmp -o data/processed_pdfs
 done
