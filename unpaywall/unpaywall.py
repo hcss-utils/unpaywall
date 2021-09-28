1d3512aa8f9ba298a826dc52a9d36bf64451eca5
hp0404 <hryhorii.pavlenko@gmail.com>
2021-09-28 16:26:00 +0300

    timeouts, connecterror; resolves #4

--- unpaywall/unpaywall.py
+++ unpaywall/unpaywall.py
@@ -85,6 +85,7 @@ class Unpaywall:
         self._ssl_context = httpx.create_ssl_context()
         self._ssl_context.options ^= ssl.OP_NO_TLSv1
         self._hooks = {"response": [self.raise_on_4xx_5xx]}
+        self._timeout = httpx.Timeout(connect=5, read=5 * 60, write=5, pool=5)
         self.raw_pdfs.mkdir(exist_ok=True, parents=True)
         self.context_created = True
 
@@ -93,11 +94,11 @@ class Unpaywall:
 
     def stream_response(self, session, endpoint):
         try:
-            with session.stream("GET", endpoint) as response:
+            with session.stream("GET", endpoint, allow_redirects=True) as response:
                 for chunk in response.iter_bytes():
                     yield chunk
         except httpx.ConnectError:
-            self.logger.info(f"Connection error: {endpoint}")
+            self.logger.info(f"Errored (connection error): {endpoint}")
 
     def download(self, session, link, filepath):
         with open(self.raw_pdfs / f"{filepath}.pdf", "wb") as out:
@@ -107,7 +108,9 @@ class Unpaywall:
     def run(self, dois):
         self.ensure_context_created()
         with httpx.Client(
-            event_hooks=self._hooks, verify=self._ssl_context, timeout=None
+            event_hooks=self._hooks,
+            verify=self._ssl_context,
+            timeout=self._timeout,
         ) as session:
             for doi, uuid in dois:
                 if self._check_exists(uuid) or self._check_attempted(uuid):
@@ -116,7 +119,6 @@ class Unpaywall:
 
                 response = self.fetch(session=session, doi=doi)
                 if response.status_code == 404:
-                    self.logger.info(response.json()["message"])
                     continue
 
                 data = response.json()
@@ -129,11 +131,7 @@ class Unpaywall:
                     link=data["best_oa_location"]["url_for_pdf"],
                     filepath=data["uuid"],
                 )
-                self.update_jsonl(
-                    data={
-                        k: v
-                        for k, v in data.items()
-                        if k not in Unpaywall.USELESS_FIELDS
-                    },
-                    filepath=self.jsonl_file,
-                )
+                updated = {
+                    k: v for k, v in data.items() if k not in Unpaywall.USELESS_FIELDS
+                }
+                self.update_jsonl(data=updated, filepath=self.jsonl_file)
